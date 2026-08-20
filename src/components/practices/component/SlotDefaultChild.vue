c55ec8abf341632e225adf4966a23842f574a90d
문희주 <creepygrape1306@gmail.com>
2026-08-20 17:44:28 +0900

    component 실습

---
+++ src/components/practices/component/SlotDefaultChild.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="base-card">
+    <slot>
+      <p>기본 콘텐츠 영역</p>
+    </slot>
+  </div>
+</template>
---
+++ src/components/practices/component/SlotNamedParent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import SlotNamedChild from './SlotNamedChild.vue'
+</script>
+
+<template>
+  <div class="practice-section">
+    <h2>Named Slot 주입 실습</h2>
+    <SlotNamedChild>
+      <template v-slot:header>
+        <h3>Child 제목 주입</h3>
+      </template>
+      <p>쓸데없는 내용....</p>
+    </SlotNamedChild>
+  </div>
+</template>
