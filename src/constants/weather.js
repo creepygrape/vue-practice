efd7ae1f518b1378fa5c570a596ff715cf67840f
문희주 <creepygrape1306@gmail.com>
2026-08-21 14:58:53 +0900

    connect openweatherapi

---
+++ src/constants/weather.js
@@ -0,0 +1,15 @@
+// src/constants/weather.js
+export const cityPool = [
+  { query: 'Seoul,KR', name: '서울' },
+  { query: 'Busan,KR', name: '부산' },
+  { query: 'Incheon,KR', name: '인천' },
+  { query: 'Daegu,KR', name: '대구' },
+  { query: 'Daejeon,KR', name: '대전' },
+  { query: 'Gwangju,KR', name: '광주' },
+  { query: 'Jeju,KR', name: '제주' },
+  { query: 'Tokyo,JP', name: '도쿄' },
+  { query: 'London,GB', name: '런던' },
+  { query: 'Paris,FR', name: '파리' },
+  { query: 'New York,US', name: '뉴욕' },
+  { query: 'Sydney,AU', name: '시드니' },
+]
